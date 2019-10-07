9b1c259a7742cd177c478b3fde20050ef43f1c90
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2019-10-06 23:46:22 -0400

    Write parsed documents to standard output

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Didactic inverted index
 
 ## Downloading data
 
-Downloading and uncompressing 10 WET files containing crawling data:
+Download and uncompress 10 WET files containing crawling data:
 
 ```bash
 $ head -n 10 wet.paths | xargs -I {} wget "https://commoncrawl.s3.amazonaws.com/{}"
@@ -12,8 +12,15 @@ $ gzip -d CC-MAIN*
 
 ## Parsing data
 
-This step processes WET documents and create intermediate postings.
+Parse WET document and save parsed words to a file.
 
 ```bash
-$ python process_wet.py CC-MAIN-20190915052433-20190915074433-00000.warc.wet > parser/CC-MAIN-20190915052433-20190915074433-00000/intermediate.postings
+$ python process_wet.py CC-MAIN-20190915052433-20190915074433-00000.warc.wet > parser.00000
+```
+
+Each line contains a URL and parsed words, separated by tabs. Example:
+
+```
+https://www.nyu.edu New York    University
+https://www.unb.br  Universidade    de  Brasília
 ```
--- process_wet.py
+++ process_wet.py
@@ -12,25 +12,77 @@ logger.setLevel(logging.INFO)
 
 
 class WETProcessor:
-    def process_wet_file(self, filename):
-        logger.info(f'Processing {filename}')
+    def __init__(self, input_filename):
+        self.input_filename = input_filename
 
-        with open(filename) as file:
-            self.process_file(file)
+        self.reader = WETReader(self.input_filename)
+        self.parser = DocumentParser()
 
-        logger.info(f'Finished processing {filename}')
+    def process(self):
+        for document in self.reader.generate_documents():
+            parsed_document = self.parser.parse_document(document)
+            if parsed_document:
+                formatted_document = self.format_document_for_output(parsed_document)
+                print(formatted_document)
 
-    def process_file(self, file):
-        while True:
-            try:
-                document = self.read_document_from_file(file)
-                document['Words'] = self.parse_words_from_lines(document['Lines'])
-            except EOFError:
-                break
+    def format_document_for_output(self, document):
+        line = []
+        line.append(document['uri'])
+        line.extend(document['words'])
+        return '\t'.join(line)
+
+
+class DocumentParser:
+    def parse_document(self, document):
+        parsed_document =  {}
+
+        if self.has_uri_header(document):
+            parsed_document['uri'] = self.get_uri_from_document(document)
+            parsed_document['words'] = self.parse_words_from_document(document)
+
+        return parsed_document
+
+    def has_uri_header(self, document):
+        return 'WARC-Target-URI' in document
+
+    def get_uri_from_document(self, document):
+        return document['WARC-Target-URI']
+
+    def parse_words_from_document(self, document):
+        return self.parse_words_from_lines(document['Content'])
+
+    def parse_words_from_lines(self, lines):
+        words = []
+
+        for line in lines:
+            words.extend(self.parse_words_from_line(line))
+
+        return words
+
+    def parse_words_from_line(self, line):
+        return re.findall(r'([\w\d]+)', line)
+
+
+class WETReader:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate_documents(self):
+        with open(self.filename) as file:
+            while True:
+                try:
+                    document = self.read_document_from_file(file)
+
+                    if not document:
+                        continue
+
+                    yield document
+                except EOFError:
+                    break
 
     def read_document_from_file(self, file):
         document = self.read_document_headers_from_file(file)
-        document['Lines'] = self.read_document_content_from_file(file)
+        document['Content'] = self.read_document_content_from_file(file)
         return document
 
     def read_document_headers_from_file(self, file):
@@ -82,26 +134,16 @@ class WETProcessor:
     def has_read_all_content(self, line):
         return line == 'WARC/1.0\n'
 
-    def parse_words_from_lines(self, lines):
-        words = []
-
-        for line in lines:
-            words.extend(self.parse_words_from_line(line))
-
-        return words
-
-    def parse_words_from_line(self, line):
-        return re.findall(r'([\w\d]+)', line)
 
 
 @click.command(
     name='process_wet',
     short_help='process WET file containing crawling data',
     context_settings={'help_option_names': ['-h', '--help']})
-@click.argument('file')
-def main(file):
-    processor = WETProcessor()
-    processor.process_wet_file(file)
+@click.argument('input')
+def main(input):
+    processor = WETProcessor(input)
+    processor.process()
 
 
 if __name__ == '__main__':
